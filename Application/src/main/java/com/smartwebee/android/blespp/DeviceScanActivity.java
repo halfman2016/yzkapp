41e2c3f1248a6bf8c32b0be84d6975d65a39acd8
halfman2020 <halfman@21cn.com>
2020-08-08 23:50:47 +0800

    open close button ok

--- Application/src/main/java/com/smartwebee/android/blespp/DeviceScanActivity.java
+++ Application/src/main/java/com/smartwebee/android/blespp/DeviceScanActivity.java
@@ -159,9 +159,9 @@ public class DeviceScanActivity extends ListActivity {
     protected void onListItemClick(ListView l, View v, int position, long id) {
         final BluetoothDevice device = mLeDeviceListAdapter.getDevice(position);
         if (device == null) return;
-        final Intent intent = new Intent(this, BleSppActivity.class);
-        intent.putExtra(BleSppActivity.EXTRAS_DEVICE_NAME, device.getName());
-        intent.putExtra(BleSppActivity.EXTRAS_DEVICE_ADDRESS, device.getAddress());
+        final Intent intent = new Intent(this, shapanctrlActivity.class);
+        intent.putExtra(shapanctrlActivity.EXTRAS_DEVICE_NAME, device.getName());
+        intent.putExtra(shapanctrlActivity.EXTRAS_DEVICE_ADDRESS, device.getAddress());
         if (mScanning) {
             mBluetoothAdapter.stopLeScan(mLeScanCallback);
             mScanning = false;
